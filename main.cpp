b3225fc036c5f43bba9837384fcead16908fa846
david <david78787878787@gmail.com>
2021-12-04 14:30:23 +0100

    Complex numbers

--- main.cpp
+++ main.cpp
@@ -1,35 +1,111 @@
 #include <iostream>
 using namespace std;
-
-class windows{
-    static string najnowsza_wersja_oprogramowania;
-    string zainstalowana_wersja_oprogramowania = "v1.0";
+const double pi = 3.14159265359;
+class complex{
 public:
-    static string opublikujNoweOprogramowanie(string nowe){
-        return najnowsza_wersja_oprogramowania = (nowe);
-    }
-    void update(){
-        zainstalowana_wersja_oprogramowania = najnowsza_wersja_oprogramowania;
-    }
+    double real;
+    double imag;
 
-string getWin() const{
-        return zainstalowana_wersja_oprogramowania;
+    complex(double X,double Y){
+          real = X;
+          imag = Y;
     }
-    void print() const{
-            cout << this->getWin()<< endl;
+    double absolute() const{  // distance between  {0,0} and z
+        double abs = this->real* this->real+this->imag* this->imag;
+        abs = sqrt(abs);
+        return abs;
     }
 
 };
-string windows::najnowsza_wersja_oprogramowania ="v2.0";
+class vector{
+      double x;
+      double y;
+public:
 
+    vector(double x,double y): x(x),y(y){
+
+    }
+    double length() const{
+        return sqrt(x*x+y*y);
+    }
+
+     vector operator+(const vector &rhs) const {
+        return {x + rhs.x, y + rhs.y};
+    }
+
+    vector &operator+=(const vector &rhs) {
+        this->x = this->x + rhs.x;
+        this->y = this->y + rhs.y;
+        return *this;
+    }
+    vector operator-(const vector &odej) const{
+        return {x-odej.x, y - odej.y};
+    }
+    vector &operator-=(const vector &odej){
+        this->x = this->x - odej.x;
+        this->y = this->y - odej.y;
+        return *this;
+    }
+    vector operator*(const vector &ilocz){
+        return {x = x*ilocz.x, y = this->y*ilocz.y};
+    }
+    vector &operator*=(const vector &ilocz) {
+        this->x = this->x * ilocz.x;
+        this->y = this->y * ilocz.y;
+        return *this;
+    }
+    vector operator==(const vector &rawn){
+        if(this->x == rawn.x && this->y == rawn.y){
+            cout << "are equal" << endl;
+        } else{
+            cout << "are not equal" << endl;
+        }
+        return *this;
+    }
+    vector operator&(const double num){ //mnozenie przez skalar
+        this->x = this->x*num;
+        this->y = this->y*num;
+        return *this;
+    }
+    vector &operator~(){
+        x = -x;
+        y = -y;
+        return *this;
+    }
+
+    void przecz(){
+        this->x = -x;
+        this->y = -y;
+    }
+    double getx(){
+        return x;
+    }
+    double gety(){
+        return y;
+    }
+    void print(){
+        cout << "vector data: " << this->x << ", " << this->y << endl;
+    }
+};
 
 int main () {
-     windows eleven;
-     eleven.print();
-     eleven.update();
-     eleven.print();
-     eleven.opublikujNoweOprogramowanie("v3.0");
-     eleven.update();
-     eleven.print();
+       vector a(4,10);
+       vector b(3,3);
+       a.print();
+       vector c = a*b;
+       c.print();
+       a.operator*(b);
+       vector e(3,3);
+       b==e;
+       a==b;
+       e.operator&(10);
+       e.operator~();
+       a.print();
+       e.print();
+       complex z(3,7);
+       cout << "Real part:" << z.real << endl;
+       cout << "imag part:" << z.imag << endl;
+      cout << z.absolute() << endl;
+
 
 }
